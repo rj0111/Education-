ca0827f896f020d7dc02932be8d193b0efc24827
rj0111 <rj0111@users.noreply.github.com>
2016-05-16 00:01:16 -0700

    Update HW6part2.java

--- HW6part2.java
+++ HW6part2.java
@@ -1,12 +1,15 @@
-public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
+public class Main {
+    // Homework 6 part 2
+    public static void main(String[] args) {
+        int address = 19986;
         int pageSize = 4096;
-        int address = 0;
-        if (address < 2) {
-        System.out.println("Enter the address: ");
-        address = in.nextInt();
-        }
+
+
         int pageNumber = address / pageSize;
         int offset = address % pageSize;
-        System.out.println("The address " + address + " contains: Page number = " + pageNumber + " And off set = " + offset);
-        }# Educaiton-
+
+        System.out.println("The address " + address + " \nThe System contains: \nPage number = " + pageNumber + " \nAnd offset = " + offset);
+
+    }
+}
